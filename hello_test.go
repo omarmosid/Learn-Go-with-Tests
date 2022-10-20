9aa7bc8af17f58275efa92b0922c9dbd2c80c174
Omar Mohammad <omarmo@cloudflare.com>
2022-10-20 14:05:03 +0100

    Make hello better

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Omar")
-	want := "Hello Omar"
+	t.Run("Saying Hello to people", func(t *testing.T) {
+		got := Hello("Omar")
+		want := "Hello Omar"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("Saying Hello World, when an empty string is passed", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello World"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("Got %s | Want %s", got, want)
 	}
 }
