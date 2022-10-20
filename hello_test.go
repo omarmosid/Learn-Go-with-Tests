0552fcf73323a8aad2f5604a971947ec8b9728e3
Omar Mohammad <omarmo@cloudflare.com>
2022-10-20 14:09:50 +0100

    Add spanish greeting

--- hello_test.go
+++ hello_test.go
@@ -4,14 +4,20 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("Saying Hello to people", func(t *testing.T) {
-		got := Hello("Omar")
+		got := Hello("Omar", "English")
 		want := "Hello Omar"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Saying Hello World, when an empty string is passed", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 }
